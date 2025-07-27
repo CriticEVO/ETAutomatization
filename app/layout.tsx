3b03feab2f736b98f8911d2b941a21f2041b0614
CriticEVO <enyo.yanakiev@gmail.com>
2025-07-27 17:33:56 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'E&T',
+  description: 'Created from E&T for E&T Automatization',
+  generator: 'et.com',
 }
 
 export default function RootLayout({
